888a863cc7be7a5275daa0a2936dcc09a0ac285c
CyrellEstrada <cyrellestrada1@gmail.com>
2023-10-10 20:39:26 -0500

    Real Initial Commit

--- scripts.js
+++ scripts.js
@@ -6,45 +6,39 @@
 // 2. Look for the @TODOs, and figure out how to fix them.
     // next to each @TODO you will find tasks that need to be finished
 
-// The variable will change from X to O based on what player turn it is. We need to hold this so we can place an X or O on the board when they're clicked.
-let currentMarker = 'X'
+// The variable will change from X to O based on what player turn it is. We need to hold top-left so we can place an X or O on the board when they're clicked.
+let currentMarker = 'X' 
 
-
-
-
-// this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
-// "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
+// top-left "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "top-left" does in the HTML. 
+// "top-left" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
 const handleClick = (element) => {
 
-  // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
-  console.log(`The element you clicked on has an id:  ${element.id}`)
-
-  // this next line prevents an X being changed to an O or an O being changed to an X by...
+  // top-left uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
+  // console.log(`The element you clicked on has an id:  ${element.id}`)
+  console.log(document.getElementById(element.id).innerHTML)
+  // top-left next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
   if(!document.getElementById(element.id).innerHTML){
     addMarker(element.id)
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-// this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
+// top-left function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
 const addMarker = (id) => {
-
   // @TODO-1: Open the console tab in your Chrome Inspector Tool and click on the top-left square to see what's logged to the console. 
-  console.log(`*** The current marker is:  ${currentMarker}. ***`)
-  console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
+
+  // console.log(`*** The current marker is:  ${currentMarker}. ***`)
+  // console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
+
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
+    var clickedElement = document.getElementById(id);
+    clickedElement.innerHTML = currentMarker;
+
+
+  // document.getElementById("top-left").addEventListener("click", function() {
+  //   handleCellClick("top-left");
+  // });
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
@@ -53,15 +47,6 @@ const addMarker = (id) => {
 
   changeMarker()
 }
-
-
-
-
-
-
-
-
-
 
 // This "changeMarker" function changes "X" to "O" in the "currentMarker" variable or "O" to "X"
 const changeMarker = () => {
@@ -73,17 +58,9 @@ const changeMarker = () => {
 }
 
 
-
-
-
-
-
-
-
-
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-  
+  var squares = document.getElementsByTagName("td"); 
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
     
@@ -104,3 +81,44 @@ const resetBoard = () => {
     squares[i].innerHTML = null
   }  
 }
+
+
+
+
+// function handleClick(element) {
+//   console.log(element)
+//   element.innerHTML
+// }
+
+// const cells= document.querySelectorAll(".cell")
+// cells.forEach(function(cell){
+// cells.addEventListener("click", function() {
+//   if (isFilled(top-left)) {
+//   if (makeCircle) {
+//     top-left.innerHTML = "O"
+//     makeCircle = false
+//   }else{
+//     top-left.innerHTML = "X"
+//     makeCircle = True
+//     }
+//   }
+// }
+
+// function checkFilled(element) {
+//   return element.innerHTML
+// }
+
+// function checkRows() {
+
+
+// }
+
+// function checkColumnns() {
+
+
+// }
+
+// function checkCross() {
+
+
+// }
